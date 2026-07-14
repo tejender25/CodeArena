d9db16ef7d5e740748d0b32501f4c2e128179edc
Tejender Singh <tsbanwala2525@gmail.com>
2026-07-14 13:30:05 +0530

    Setup application routes

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const jobRoutes = require("./routes/jobRoutes");
-
+const applicationRoutes = require("./routes/applicationRoutes");
 const app = express();
 
 app.use(cors());
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/jobs", jobRoutes);
+app.use("/api/applications", applicationRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({
