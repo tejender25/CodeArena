ec13586edaa6edfb1e4954044e34b6cf0885cf06
Tejender Singh <tsbanwala2525@gmail.com>
2026-07-14 13:28:58 +0530

    Create application model

---
+++ server/models/Application.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const applicationSchema = new mongoose.Schema({
+
+    job: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+        required: true
+    },
+
+    applicant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["Pending", "Accepted", "Rejected"],
+        default: "Pending"
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Application", applicationSchema);
